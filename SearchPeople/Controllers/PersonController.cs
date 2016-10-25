ab6b84876de5c021a4f85bcf3b274d1f108521f0
palmerc9 <christian_palmer@byu.edu>
2016-10-24 22:02:05 -0600

    Edited

--- SearchPeople/Controllers/PersonController.cs
+++ SearchPeople/Controllers/PersonController.cs
@@ -32,105 +32,89 @@ namespace SearchPeople.Controllers
             return Json(searchAsList, JsonRequestBehavior.AllowGet);
         }
 
-        //***************** Uncomment for CRUD capabilities ********************
-        //
-        //public ActionResult Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Person person = db.Persons.Find(id);
-        //    if (person == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(person);
-        //}
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-        //// GET: Person/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
+        // POST: Person/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,lastName,firstName,age,address,interests,profilePicture")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Persons.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-        //// POST: Person/Create
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "ID,lastName,firstName,age,address,interests,profilePicture")] Person person)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Persons.Add(person);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
+            return View(person);
+        }
 
-        //    return View(person);
-        //}
+        // GET: Person/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.Persons.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
 
-        //// GET: Person/Edit/5
-        //public ActionResult Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Person person = db.Persons.Find(id);
-        //    if (person == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(person);
-        //}
+        // POST: Person/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,lastName,firstName,age,address,interests,profilePicture")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
 
-        //// POST: Person/Edit/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "ID,lastName,firstName,age,address,interests,profilePicture")] Person person)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(person).State = EntityState.Modified;
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(person);
-        //}
+        // GET: Person/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.Persons.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
 
-        //// GET: Person/Delete/5
-        //public ActionResult Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Person person = db.Persons.Find(id);
-        //    if (person == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(person);
-        //}
+        // POST: Person/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Person person = db.Persons.Find(id);
+            db.Persons.Remove(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-        //// POST: Person/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult DeleteConfirmed(int id)
-        //{
-        //    Person person = db.Persons.Find(id);
-        //    db.Persons.Remove(person);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
-
-        //protected override void Dispose(bool disposing)
-        //{
-        //    if (disposing)
-        //    {
-        //        db.Dispose();
-        //    }
-        //    base.Dispose(disposing);
-        //}
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
