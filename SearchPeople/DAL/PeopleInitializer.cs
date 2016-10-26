181f3f6b8db03a2284edb8189f08b0250780fc79
palmerc9 <christian_palmer@byu.edu>
2016-10-25 19:12:03 -0600

    Fixed Errors

--- SearchPeople/DAL/PeopleInitializer.cs
+++ SearchPeople/DAL/PeopleInitializer.cs
@@ -13,11 +13,11 @@ namespace SearchPeople.DAL
         {
             var testPeople = new List<Person>()
             {
-                new Person{ID = 1, lastName = "Palmer", firstName="Chris", address="123 Street", age=22, interests="Sports",profilePicture= null},
-                new Person{ID=2, lastName="Smith",firstName="Joe", address="321 Road", age=35, interests="Reading", profilePicture= null},
-                new Person{ID=3, lastName="Smith",firstName="Henry", address="1 Infinite Loop", age=21, interests="Programming", profilePicture=null},
-                new Person{ID=4, lastName="Schmoe",firstName="Joe", address="Boulevard of Broken Dreams", age=45, interests="Fishing", profilePicture=null},
-                new Person{ID=5, lastName="Johns",firstName="Jimmy", address="Stairway to Heaven", age=30, interests="Surfing", profilePicture=null}
+                new Person{ID = 1, lastName = "Palmer", firstName="Chris", address="123 Street", age=22, interests="Sports",profilePicture= "/Content/stick 1.png"},
+                new Person{ID=2, lastName="Smith",firstName="Joe", address="321 Road", age=35, interests="Reading", profilePicture= "/Content/stick 2.jpg"},
+                new Person{ID=3, lastName="Smith",firstName="Henry", address="1 Infinite Loop", age=21, interests="Programming", profilePicture="/Content/stick 3.png"},
+                new Person{ID=4, lastName="Schmoe",firstName="Joe", address="Boulevard of Broken Dreams", age=45, interests="Fishing", profilePicture="/Content/stick 4.jpg"},
+                new Person{ID=5, lastName="Johns",firstName="Jimmy", address="Stairway to Heaven", age=30, interests="Surfing", profilePicture="/Content/stick 5.png"}
 
             };
 
